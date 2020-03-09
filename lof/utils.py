20e37c8a3a98d5248278b14303d27823185e8af3
refraction-ray <kcanamgal@foxmail.com>
2020-03-09 13:39:38 +0800

    time check and cron

---
+++ lof/utils.py
@@ -0,0 +1,24 @@
+import xalpha as xa
+import datetime as dt
+
+
+def is_cn_trading(dtobj=None):
+    tz_bj = dt.timezone(dt.timedelta(hours=8))
+    if not dtobj:
+        dtobj = dt.datetime.now(tz=tz_bj)
+    ison = xa.cons.caldate[
+        xa.cons.caldate["cal_date"] == dtobj.strftime("%Y-%m-%d")
+    ].iloc[0]["is_open"]
+    if ison == 0:
+        return False
+    else:  # 交易日
+        if (
+            dtobj.hour < 9
+            or dtobj.hour >= 15
+            or (dtobj.hour == 9 and dtobj.minute < 30)
+            or (dtobj.hour == 12)
+            or (dtobj.hour == 11 and dtobj.minute > 30)
+        ):
+            return False
+        else:  # 交易日交易时间
+            return True
--- scripts/premium.py
+++ scripts/premium.py
@@ -5,9 +5,13 @@ here = os.path.abspath(__file__)
 sys.path.insert(0, os.path.dirname(os.path.dirname(here)))
 
 from lof.examples import pred_ntf_oil
+from lof.utils import is_cn_trading
 
 
 def main(*argv):
+    if not is_cn_trading():
+        print("非交易时间")
+        return
     for code in ["SH501018", "SZ160216", "SZ162411"]:
         pred_ntf_oil(code, token=argv[1])
 
