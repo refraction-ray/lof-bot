d962e0c81c38719c4c13e877827b0edf1784592e
refraction-ray <kcanamgal@foxmail.com>
2020-03-09 13:17:45 +0800

    add examples

---
+++ lof/examples.py
@@ -0,0 +1,29 @@
+import xalpha as xa
+
+from .predict import get_qdii_t
+from .holdings import holdings
+from .notification import notify
+
+
+def pred_ntf_oil(code, **kws):
+    daily_holdings = kws.get("daily_holdings", holdings[code[2:]])
+    rt_holdings = kws.get("rt_holdings", holdings["oil_rt"])
+    ddprice, dprice = get_qdii_t(code, daily_holdings, rt_holdings)
+    print(code, dprice, ddprice)
+    r = xa.get_rt(code)
+    cprice = r["current"]
+    higher = kws.get("h", 1.04)
+    lower = kws.get("l", 0.97)
+    _type = kws.get("ntf_type", "pb")
+    if cprice / dprice > higher or cprice / dprice < lower:
+        notify(
+            r["name"],
+            "溢价率已达到 %s%%。T-1 日净值预估 %s, T 日净值实时预估 %s"
+            % (
+                round((cprice / dprice - 1) * 100, 1),
+                round(ddprice, 3),
+                round(dprice, 3),
+            ),
+            token=kws.get("token"),
+            _type=_type,
+        )
--- scripts/premium.py
+++ scripts/premium.py
@@ -4,28 +4,12 @@ import os
 here = os.path.abspath(__file__)
 sys.path.insert(0, os.path.dirname(os.path.dirname(here)))
 
-import lof.predict as lp
-from lof.holdings import holdings
-from lof.notification import notify
-import xalpha as xa
+from lof.examples import pred_ntf_oil
 
 
 def main(*argv):
-    ddprice, dprice = lp.get_qdii_t(
-        "SH501018", holdings["501018"], holdings["oil_rt"]
-    )
-    cprice = xa.get_rt("SH501018")["current"]
-    if cprice / dprice > 1.05:
-        notify(
-            "南方原油",
-            "溢价率已达到 %s。T-1 日净值预估 %s, T 日净值实时预估 %s"
-            % (
-                round((cprice / dprice - 1) * 100, 1),
-                round(ddprice, 3),
-                round(dprice, 3),
-            ),
-            token=argv[1],
-        )
+    for code in ["SH501018", "SZ160216", "SZ162411"]:
+        pred_ntf_oil(code, token=argv[1])
 
 
 if __name__ == "__main__":
