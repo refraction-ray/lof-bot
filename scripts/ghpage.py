b439b5ae00a4b9d1abcdfcf1f5beba1c23865076
refraction-ray <kcanamgal@foxmail.com>
2020-03-10 16:24:10 +0800

    render via the cloud

--- scripts/ghpage.py
+++ scripts/ghpage.py
@@ -5,20 +5,26 @@ here = os.path.abspath(__file__)
 project_dir = os.path.dirname(os.path.dirname(here))
 sys.path.insert(0, project_dir)
 
+import requests
 from lof.gh import render
+
+codes = ["SH501018"]
 
 
 def main():
-    fs = [
-        f
-        for f in os.listdir(project_dir)
-        if f.startswith("SH") or f.startswith("SZ")
-    ]
-    for f in fs:
-        with open(os.path.join(project_dir, f), "r") as fh:
-            text = fh.read()
-        with open(os.path.join(project_dir, f), "w") as fh:
-            fh.writelines([render(text, code=f.split(".")[0])])
+    # fs = [
+    #     f
+    #     for f in os.listdir(project_dir)
+    #     if f.startswith("SH") or f.startswith("SZ")
+    # ]
+    for code in codes:
+        r = requests.get(
+            "https://raw.githubusercontent.com/refraction-ray/lof-bot/gh-pages/%s.html"
+            % code
+        )
+
+        with open(os.path.join(project_dir, code + ".html"), "w") as fh:
+            fh.writelines([render(r.text, code=code)])
 
 
 if __name__ == "__main__":
